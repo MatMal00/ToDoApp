9e3d5a3b0e755ef05363f338a950d445296f33e0
Mateusz Malec <mateusz.malec@microsoft.wsei.edu.pl>
2022-05-16 19:34:22 +0200

    feat: add basic layout for TasksToDo view

--- ToDoApp/Controls/ToDoTask.xaml.cs
+++ ToDoApp/Controls/ToDoTask.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ToDoApp.Controls
+namespace ToDoApp
 {
     /// <summary>
     /// Interaction logic for ToDoTask.xaml
