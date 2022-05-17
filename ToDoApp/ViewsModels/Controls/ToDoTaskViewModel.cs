91b1c507e60714127112216c91e4fb4abacf2d8e
Mateusz Malec <mateusz.malec@microsoft.wsei.edu.pl>
2022-05-17 20:17:23 +0200

    feat: style correction

--- ToDoApp/ViewsModels/Controls/ToDoTaskViewModel.cs
+++ ToDoApp/ViewsModels/Controls/ToDoTaskViewModel.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace ToDoApp
 {
     public class ToDoTaskViewModel: BaseTask
     {
+        public ICommand MarkAsDoneTaskCommand { get; set; }
+
+        public ToDoTaskViewModel()
+        {
+            MarkAsDoneTaskCommand = new RelayCommand(MarkAsDoneTask);
+        }
+
+        private void MarkAsDoneTask()
+        {
+            MessageBox.Show(TaskId.ToString());
+        }
     }
 }
